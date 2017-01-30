7ea9e87a517b82d811f4e1cc4679260417ef70bf
nokout <nigel.okeefe@gmail.com>
2017-01-31 01:24:31 +1100

    add mixed changes to branch for breaking into specific fixes later

--- src/app/app.main.ts
+++ src/app/app.main.ts
@@ -2,8 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main',
-  templateUrl: './app.main.html',
-  styleUrls: ['./app.main.css']
+  templateUrl: './app.main.html'
 })
 export class MainComponent {
 }

a8076b8e22804e1366be9002662f64c08ff14cb2
nokout <nigel.okeefe@gmail.com>
2017-02-14 12:53:35 +1100

    move to jekyll

--- src/app/app.main.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-main',
-  templateUrl: './app.main.html',
-  styleUrls: ['./app.main.css']
-})
-export class MainComponent {
-}
